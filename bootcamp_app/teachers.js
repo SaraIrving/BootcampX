193c456216d6a09e54e80fb28b1af4f43e25333f
Sara Irving <sarakirving@gmail.com>
2020-07-23 03:26:11 +0000

    Code to connect teachers.js to database and query for the teachers names who are associated with a cohort which is provided by command line input

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query (`
+SELECT DISTINCT teachers.name AS teachers, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests
+ON teachers.id = teacher_id
+JOIN students
+ON student_id = students.id
+JOIN cohorts
+ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teachers}`);
+  })
+});
